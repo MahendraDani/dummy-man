acf56bbed8bf290cc6458370403efabeff5889b9
Mahendra Dani <danimahendra0904@gmail.com>
2024-11-21 14:04:54 +0530

    remove /extension

--- extension/entrypoints/background.ts
+++
@@ -1,5 +0,0 @@
-import {browser,Tabs} from "wxt/browser"
-
-export default defineBackground(() => {
-  console.log('Hello background!', { id: browser.runtime.id });
-});
--- extension/entrypoints/content/Modal.tsx
+++
@@ -1,7 +0,0 @@
-export const Modal = ({onClose} : {onClose : ()=> void})=>{
-  return (
-    <div className="w-[36rem] h-[28rem] bg-white">
-      <p>Hello </p>
-    </div>
-  )
-}
